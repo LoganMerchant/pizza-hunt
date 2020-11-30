9dfac23170150ea85a648a28ba31ee7ad85da16c
Logan Merchant <merchantclogan@gmail.com>
2020-11-29 21:09:15 -0700

    add pizza model, connect server to mongoose

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,5 +9,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+// Use heroku uri, or the localhost
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+    // Make update and remove methods use native findOneAndUpdate()
+    useFindAndModify: false,
+    // Used as a fallback to the old MongoDB driver if necessary
+    useNewUrlParser: true,
+    // Opt in to the new MongoDB driver
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
