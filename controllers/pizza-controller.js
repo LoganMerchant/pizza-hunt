6b36715c468c801eef56667c08a87733ac93e13b
Logan Merchant <merchantclogan@gmail.com>
2020-12-03 16:49:19 -0700

    add validation for pizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,78 +1,81 @@
-const { Pizza } = require('../models');
+const { Pizza } = require("../models");
 
 const pizzaController = {
-    // get all pizzas
-    getAllPizza(req, res) {
-        Pizza.find({})
-            // populate the comments field with data, not just it's _id
-            .populate({
-                path: 'comments',
-                select: '-__v'
-            })
-            // reads like "minus the `__v` property"
-            .select('-__v')
-            // return the newest pizza first
-            .sort({ _id: -1 })
-            .then(dbPizzaData => res.json(dbPizzaData))
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
-    },
-    // get one pizza by id
-    getPizzaById({ params }, res) {
-        Pizza.findOne({ _id: params.id })
-            .populate({
-                path: 'comments',
-                select: '-__v'
-            })
-            .select('-__v')
-            .then(dbPizzaData => {
-                // If no pizza is found, send 404
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id!' });
-                    return;
-                };
-                
-                res.json(dbPizzaData);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
-    },
-    // create a new pizza
-    createPizza({ body }, res) {
-        Pizza.create(body)
-            .then(dbPizzaData => res.json(dbPizzaData))
-            .catch(err => res.status(400).json(err));
-    },
-    // update pizza by id, return the updated document rather than the original
-    updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
-            .then(dbPizzaData => {
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id! '});
-                    return;
-                };
+  // get all pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      // populate the comments field with data, not just it's _id
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      // reads like "minus the `__v` property"
+      .select("-__v")
+      // return the newest pizza first
+      .sort({ _id: -1 })
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // get one pizza by id
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbPizzaData) => {
+        // If no pizza is found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
 
-                res.json(dbPizzaData);
-            })
-            .catch(err => res.status(400).json(err));
-    },
-    // delete a pizza by id
-    deletePizza({ params }, res) {
-        Pizza.findByIdAndDelete({ _id: params.id })
-            .then(dbPizzaData => {
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id! '});
-                    return;
-                };
+        res.json(dbPizzaData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // create a new pizza
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => res.status(400).json(err));
+  },
+  // update pizza by id, return the updated document rather than the original
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id! " });
+          return;
+        }
 
-                res.json(dbPizzaData);
-            })
-            .catch(err => res.status(400).json(err));
-    }
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  // delete a pizza by id
+  deletePizza({ params }, res) {
+    Pizza.findByIdAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id! " });
+          return;
+        }
+
+        res.json(dbPizzaData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = pizzaController;
